acbf3c2e214fafcf101844d2e0ca0fb93a8040bd
Anil Chowdhury <anil.chowdhury@harness.io>
2021-01-04 18:52:04 +0530

    Update azure.tf

--- azure.tf
+++ azure.tf
@@ -17,14 +17,14 @@ provider "azurerm" {
   features {}
 }
 # Create a resource group
-resource "azurerm_resource_group" "anilterraformfrg" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "example" {
+  name     = "anil-terraform-resourceGroup"
   location = "West Europe"
 }
 # Create a virtual network within the resource group
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
-  resource_group_name = azurerm_resource_group.anilterraformfrg.name
-  location            = azurerm_resource_group.anilterraformfrg.location
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
   address_space       = ["10.0.0.0/16"]
 }
