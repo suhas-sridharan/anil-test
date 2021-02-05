da5a4ce5734dfd0a69915240aefec698d83e1ef9
Anil Chowdhury <anil.chowdhury@harness.io>
2021-02-05 12:18:24 +0530

    Update main.tf

--- terraformAppService/slotOutput/main.tf
+++ terraformAppService/slotOutput/main.tf
@@ -59,6 +59,13 @@ resource "azurerm_app_service" "main" {
   }
 }
 
+resource "azurerm_virtual_network" "main" {
+  name                = "example-network"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  address_space       = ["10.0.0.0/16"]
+}
+
 output "subId" {
   value = "${var.subscription_id}"
 }
