2376a9f99d4888ba91e73b7a610cf201a4f42d00
Anil Chowdhury <anil.chowdhury@harness.io>
2021-02-05 12:35:21 +0530

    Update main.tf

--- terraformAppService/slotOutput/main.tf
+++ terraformAppService/slotOutput/main.tf
@@ -59,6 +59,30 @@ resource "azurerm_app_service" "main" {
   }
 }
 
+resource "azurerm_app_service_slot" "main" {
+  name                = "terraformStage"
+  app_service_name    = azurerm_app_service.main.name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  app_service_plan_id = azurerm_app_service_plan.main.id
+
+  site_config {
+    linux_fx_version = "DOCKER|mcr.microsoft.com/appsvc/staticsite:latest"
+    always_on        = "true"
+  }
+  
+  app_settings = {
+    "stage_key" = "stage_value"
+     WEBSITES_ENABLE_APP_SERVICE_STORAGE = false
+  }
+
+  connection_string {
+    name  = "Database"
+    type  = "SQLServer"
+    value = "Server=some-server.mydomain.com;Integrated Security=SSPI-stage"
+  }
+}
+
 
 # resource "azurerm_virtual_network" "main" {
 #   name                = "example-network"
