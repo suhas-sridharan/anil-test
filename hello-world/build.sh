758141420ba3531105b13a6faf0a9f9386716e1e
Anil Chowdhury <anil.chowdhury@harness.io>
2021-08-31 00:29:46 +0530

    Create build.sh

---
+++ hello-world/build.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+
+echo "### Create helloworld.war file"
+echo
+echo "Calling jar"
+echo
+jar -cvf helloworld.war *.jsp WEB-INF
+echo
+echo "Moving helloworld.war to dist/ with option --FORCE"
+mv -f helloworld.war dist/
+echo
+echo "Done"
